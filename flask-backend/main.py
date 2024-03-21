93b62d15ce32e322039aa6389ae70616a7d39f80
Denzel Kanyeki <denzelkinyua11@gmail.com>
2024-03-21 23:33:13 +0300

    Fourth Commit, CRUD API endpoints added
    CRUD API endpoints were added, but with a pass exception to it. I'll add those functionalities tomorrow.
    I love Python.

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import DevelopmentConfig
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, fields
 from models import db, Recipe
 
 
@@ -9,10 +9,22 @@ app.config.from_object(DevelopmentConfig)
 db.init_app(app)
 api = Api(app, doc="/docs")
 
+# Models {Serializer} transforms data to JSON format
+
+recipe_models = api.model(
+    "Recipe",
+    {
+        "id": fields.Integer(),
+        "title": fields.String(),
+        "description": fields.String()
+    }
+)
+
 
 # First API route
 @api.route('/hello')
 class HelloResource(Resource):
+
     def get(self):
         message = {
             "sender": "deecodes",
@@ -21,6 +33,30 @@ class HelloResource(Resource):
         }
         return message
 
+# CRUD endpoints
+    
+@api.route("/recipes")
+class RecipesResource(Resource):
+    def get(self):
+        # Get a recipe
+        pass
+    def post(self):
+        # Post a recipe
+        pass
+
+@api.route("/recipe/<int:id>")
+class RecipeResource(Resource):
+    def get(self, id):
+        # Get a recipe by a specified ID
+        pass
+
+    def update(self, id):
+        # Update a recipe by a specified ID
+        pass
+
+    def delete(self, id):
+        # Delete a recipe by a specified ID
+        pass
 
 # Shell Configuration
 @app.shell_context_processor
@@ -29,6 +65,7 @@ def make_shell_context():
         "db": db,
         "Recipe": Recipe
     }   
+
 
 if __name__ == "__main__":
     app.run()
