5c768ea74efd9138e6b191decf451a1e2c1bc139
unknown <denzelkinyua11@gmail.com>
2024-03-24 22:14:09 +0300

    Seventh Commit - Addition of a User model for logging in

--- flask-backend/models.py
+++ flask-backend/models.py
@@ -24,3 +24,12 @@ class Recipe(db.Model):
         self.description = description
 
         db.session.commit()
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(25), nullable=False, unique=True)
+    email = db.Column(db.String(25), nullable=False, unique=True)
+    password = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
