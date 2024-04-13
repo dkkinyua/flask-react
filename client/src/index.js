74bfdf04986599b5ee1c33aaded0ca1e93fe8f21
unknown <denzelkinyua11@gmail.com>
2024-04-13 11:36:07 +0300

    Commit: Created a components file and a navbar.js to hold our navbar. Update to index.js in our App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import NavBar from './components/navbar';
 
 const App = () => {
-    useEffect(() => {
-        fetch("/recipe/hello")
-        .then(response => response.json())
-        .then(data => {console.log(data)
-            setMessage(data.message)
-        })
-        .catch(err => console.log(err))
-    }, []);
 
-    const [message, setMessage] = useState("");
     return (
-        <div>
-            {message}
+        <div className='container'>
+            <NavBar/>
         </div>
     )
 }
 
-const root = ReactDOM.render(<App/>, document.getElementById('root'));
-root.render(<App/>)
+ReactDOM.render(<App/>,document.getElementById('root'));
+
