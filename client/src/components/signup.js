97badd74a96048c8bcf92b20c3c512e863843fa8
unknown <denzelkinyua11@gmail.com>
2024-04-16 00:34:22 +0300

    Commit: Added functionality to the signup form using functions like onChange, onClick and logging outcome to the console

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Form, Button } from 'react-bootstrap';
 
 const SignUp = () => {
@@ -7,6 +7,10 @@ const SignUp = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+
+    const submitForm = () => {
+        console.log("Form Submitted sucessfully!")
+    }
 
 
     return (
@@ -18,25 +22,25 @@ const SignUp = () => {
                 <Form>
                     <Form.Group className="mb-2">
                         <Form.Label>Username:</Form.Label>
-                        <Form.Control type='text' placeholder="Username" value={username} name="username" />
+                        <Form.Control type='text' placeholder="Username" value={username} name="username" onChange={(e) => { setUsername(e.target.value) }} />
                     </Form.Group>
                     <Form.Group className="mb-2">
                         <Form.Label >Email Address:</Form.Label>
-                        <Form.Control type='email' placeholder="Your Email Address" value={email} name="email" />
+                        <Form.Control type='email' placeholder="Your Email Address" value={email} name="email" onChange={(e) => { setEmail(e.target.value) }} />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-2">
                         <Form.Label>Password:</Form.Label>
-                        <Form.Control type='password' placeholder="Password" value={password} name="password" />
+                        <Form.Control type='password' placeholder="Password" value={password} name="password" onChange={(e) => { setPassword(e.target.value) }} />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className="mb-2">Confirm Password:</Form.Label>
-                        <Form.Control type='password' placeholder="Confirm your password." value={confirmPassword} name="confirmPassword" />
+                        <Form.Control type='password' placeholder="Confirm your password." value={confirmPassword} name="confirmPassword" onChange={(e) => { setConfirmPassword(e.target.value) }} />
                     </Form.Group>
                     <Form.Group>
-                        <Button as="sub" variant="primary" className="mt-2">
+                        <Button as="sub" variant="primary" className="mt-2" onClick={submitForm}>
                             Sign Up
                         </Button>
                     </Form.Group>
