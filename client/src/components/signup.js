55ef1d171f6ecacb09d7f21b02f40eeed9f9d9fb
unknown <denzelkinyua11@gmail.com>
2024-04-17 12:16:21 +0300

    Commit: Signup functionalities and connecting to server successful. Added more form validation i.e. if passwords match with the password on the confirmPassword field.

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -5,10 +5,36 @@ import { useForm } from 'react-hook-form'
 
 const SignUp = () => {
 
-    const {register, watch, handleSubmit, formState:{errors}} = useForm();
+    const { register, watch, handleSubmit, reset, formState: { errors } } = useForm();
 
     const submitForm = (data) => {
-        console.log(data)
+        if (data.password === data.confirmPassword) {
+            const body = {
+                username: data.username,
+                email: data.email,
+                password: data.password
+            }
+
+            const requestOptions = {
+                method: "POST",
+                headers: {
+                    'content-type': "application/json"
+                },
+                body: JSON.stringify(body)
+            }
+
+            fetch('/auth/signup', requestOptions)
+
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+
+            alert("User has been created, you can now log in.")
+            reset()
+        }
+        else{
+            alert("Passwords don't match, try again.")
+        }
     }
 
     // console.log(watch("username"))
@@ -22,12 +48,12 @@ const SignUp = () => {
                 <Form>
                     <Form.Group className="mb-2">
                         <Form.Label>Username:</Form.Label>
-                        <Form.Control type='text' placeholder="Username" {...register("username", {required:true, maxLength:20})} />
+                        <Form.Control type='text' placeholder="Username" {...register("username", { required: true, maxLength: 20 })} />
                         {errors.username && <span className="errors">Username is required</span>}
                     </Form.Group>
                     <Form.Group className="mb-2">
                         <Form.Label >Email Address:</Form.Label>
-                        <Form.Control type='email' placeholder="Your Email Address" {...register("email", {required:true, maxLength:50})} />
+                        <Form.Control type='email' placeholder="Your Email Address" {...register("email", { required: true, maxLength: 50 })} />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
@@ -36,14 +62,14 @@ const SignUp = () => {
                     </Form.Group>
                     <Form.Group className="mb-2">
                         <Form.Label>Password:</Form.Label>
-                        <Form.Control type='password' placeholder="Password" {...register("password", {required:true, minLength:8})} />
+                        <Form.Control type='password' placeholder="Password" {...register("password", { required: true, minLength: 8 })} />
                         {errors.password && <span className="errors">Password is required.</span>}
                         <br></br>
                         {errors.password && errors.password.type === "minLength" && <span className="errors">Password should be 8 characters long.</span>}
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className="mb-2">Confirm Password:</Form.Label>
-                        <Form.Control type='password' placeholder="Confirm your password." {...register("confirmPassword", {required:true, minLength:8})} />
+                        <Form.Control type='password' placeholder="Confirm your password." {...register("confirmPassword", { required: true, minLength: 8 })} />
                         {errors.confirmPassword && <span className="errors">This field is required.</span>}
                     </Form.Group>
                     <Form.Group>
