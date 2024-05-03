d01cffaba1995e3e8e826bba2cecd28a00a6892c
unknown <denzelkinyua11@gmail.com>
2024-05-03 23:24:35 +0300

    Commit: Using useAuth hook from react-token-auth (see forked repo) to separate what a logged user and a logged out user would see on the home page of our app.

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,23 +1,43 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import { Container, Col, Row, Button } from 'react-bootstrap'
+import { useAuth } from "../auth";
+
+
 
 const Home = () => {
-    return (
-        <Container>
-            <Row>
-                <Col md={6}>
-                    <div className="mt-3">
-                        <h1>Welcome to the React Recipes App</h1>
-                    </div>
-                </Col>
-                <Col md={6}>
-                    <Link to="/signup"><Button variant="primary">Sign Up</Button></Link>
-                    <Link to="/login"><Button variant="primary" className="mx-2">Login Here</Button></Link>
-                </Col>
-            </Row>
-        </Container>
+    const LoggedInHome = () => {
+        return (
+            <div className="recipes">
+                <h1>
+                    Recipes will be here!
+                </h1>
+            </div>
+        )
+    }
 
+    const LoggedOutHome = () => {
+        return (
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <div className="mt-3">
+                            <h1>Welcome to the React Recipes App</h1>
+                        </div>
+                    </Col>
+                    <Col md={6}>
+                        <Link to="/signup"><Button variant="primary">Sign Up</Button></Link>
+                        <Link to="/login"><Button variant="primary" className="mx-2">Login Here</Button></Link>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+    const [logged] = useAuth()
+    return (
+        <>
+            {logged ? <LoggedInHome /> : <LoggedOutHome />}
+        </>
     )
 }
 
