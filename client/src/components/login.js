7105f5e9a1080ce603c999c51e2f10e3b96ee364
unknown <denzelkinyua11@gmail.com>
2024-05-03 13:55:06 +0300

    Commit: Starting off on the login page, made small tweaks on the signup.js errors.

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -24,16 +24,18 @@ const Login = () => {
                         <Form.Control type='text' placeholder="Username"
                          {...register('username', {required:true, maxLength:25})}
                           />
-                        {errors.username && <span className="errors">Username is required</span>}
-                        {errors.username && <span className="errors">Maximum charaters reached: 25</span>}
+                        {errors.username && errors.username.type === "required" && <span className="errors">Username is required</span>}
+                        <br/>
+                        {errors.username && errors.username.type === "maxLength" && <span className="errors">Maximum characters reached: 25</span>}
                     </Form.Group>
                     <Form.Group className="mb-2">
                         <Form.Label>Password:</Form.Label>
                         <Form.Control type='password' placeholder="Password"
                         {...register("password", {required:true, minLength:8})}
                         />
-                        {errors.password && <span className="errors">Password is required</span>}
-                        {errors.password && <span className="errors">Minimum characters required: 8</span>}
+                        {errors.password && errors.password.type === "required" && <span className="errors">Password is required</span>}
+                        <br/>
+                        {errors.password && errors.password.type == "required" && <span className="errors">Minimum characters required: 8</span>}
                     </Form.Group>
                     <Form.Group>
                         <Button as="sub" variant="primary" className="mt-2" onClick={handleSubmit(loginForm)}>
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -50,10 +50,7 @@ const SignUp = () => {
                 {show ?
                 <>
                     <Alert variant="success" onClose={() => setShow(false)} dismissible>
-                        <Alert.Heading>Hooray!</Alert.Heading>
-                        <p>
-                            {serverResponse}
-                        </p>
+                        <Alert.Heading>{ serverResponse }</Alert.Heading>
                     </Alert>
                     <h1>
                         Sign Up Here
@@ -69,7 +66,8 @@ const SignUp = () => {
                     <Form.Group className="mb-2">
                         <Form.Label>Username:</Form.Label>
                         <Form.Control type='text' placeholder="Username" {...register("username", { required: true, maxLength: 20 })} />
-                        {errors.username && <span className="errors">Username is required</span>}
+                        {errors.username && errors.username.type === "required" && <span className="errors">Username is required</span>}
+                        {errors.username && errors.username.type === "maxLength" && <span className="errors">Maximum characters reached: 20</span>}
                     </Form.Group>
                     <Form.Group className="mb-2">
                         <Form.Label >Email Address:</Form.Label>
@@ -78,19 +76,19 @@ const SignUp = () => {
                             We'll never share your email with anyone else.
                         </Form.Text>
                         <br></br>
-                        {errors.email && <span className="errors">Email is required</span>}
+                        {errors.email && errors.email.type === "required" && <span className="errors">Email is required</span>}
                     </Form.Group>
                     <Form.Group className="mb-2">
                         <Form.Label>Password:</Form.Label>
                         <Form.Control type='password' placeholder="Password" {...register("password", { required: true, minLength: 8 })} />
-                        {errors.password && <span className="errors">Password is required.</span>}
+                        {errors.password && errors.password.type === "required" && <span className="errors">Password is required.</span>}
                         <br></br>
                         {errors.password && errors.password.type === "minLength" && <span className="errors">Password should be 8 characters long.</span>}
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className="mb-2">Confirm Password:</Form.Label>
                         <Form.Control type='password' placeholder="Confirm your password." {...register("confirmPassword", { required: true, minLength: 8 })} />
-                        {errors.confirmPassword && <span className="errors">This field is required.</span>}
+                        {errors.confirmPassword && errors.confirmPassword.type === "required" && <span className="errors">This field is required.</span>}
                     </Form.Group>
                     <Form.Group>
                         <Button as="sub" variant="primary" className="mt-2" onClick={handleSubmit(submitForm)}>
